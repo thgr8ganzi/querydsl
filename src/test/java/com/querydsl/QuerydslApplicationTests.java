fa56046679611589736f41cebafd6bd4e9806a20
leejisoo <leejisoo@atnsgroup.com>
2022-06-10 16:27:33 +0900

    QueryDsl 연습예제 입니다.

---
+++ src/test/java/com/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,38 @@
+package com.querydsl;
+
+import com.querydsl.entity.Hello;
+import com.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+
+    }
+
+}
